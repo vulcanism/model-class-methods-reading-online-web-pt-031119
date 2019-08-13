cc69af2167efb89e2ccb3848269a860b860c86bb
Savannah C <sgcorrell@gmail.com>
2019-08-13 15:34:47 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,11 @@ class PostsController < ApplicationController
     if !params[:author].blank?
       @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
-    
-    if params[:date] == "Today"
-      @posts = Post.from_today
-    else
-      @posts = Post.old_news
-    end
-    
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
     else
     # if no filters are applied, show all posts
       @posts = Post.all
